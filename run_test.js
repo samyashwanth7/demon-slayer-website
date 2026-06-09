c4ca32cd468843e569f99c90de2eedfb2b5f07c4
Sampath Kumar Pilla <pillasampathkumar@gmail.com>
2026-06-10 00:11:27 +0530

    Delete run_test.js

--- run_test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const jsdom = require('jsdom');
-const html = fs.readFileSync('index.html', 'utf8');
-
-const dom = new jsdom.JSDOM(html, { 
-  runScripts: 'dangerously'
-});
-
-setTimeout(() => console.log('Done test'), 1500);
