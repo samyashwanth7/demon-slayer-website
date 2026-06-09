d1c8db1976736c32f429fffe56c11671e4351435
Sampath Kumar Pilla <pillasampathkumar@gmail.com>
2026-06-10 00:10:27 +0530

    Delete extract.js

--- extract.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const scriptMatch = html.match(/<script type="module">([\s\S]*?)<\/script>/);
-fs.writeFileSync('script.js', scriptMatch[1]);
-console.log('Script written.');
