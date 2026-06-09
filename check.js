22a79470ac4ddedb6d27b7531d6f61572b376a09
Sampath Kumar Pilla <pillasampathkumar@gmail.com>
2026-06-10 00:10:13 +0530

    Delete check.js

--- check.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const matches = [...html.matchAll(/src="([^"]+\.(png|jpg|jpeg))"/g)];
-const images = matches.map(m => m[1]).filter((v, i, a) => a.indexOf(v) === i);
-console.log('Images referenced in HTML:');
-images.forEach(img => {
-  const exists = fs.existsSync(img);
-  console.log(`- ${img}: ${exists ? 'EXISTS' : 'MISSING'}`);
-});
